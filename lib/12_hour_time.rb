54d51018f2720c76e043036dd00bf81bd687f04a
maurice.aubrey <maurice.aubrey@af405dde-7b3e-0410-a8e9-8b16305dd6c8>
2008-04-25 21:57:12 +0000

    first cut at updating for edge; untested
    git-svn-id: http://rails-twelve-hour-time-plugin.googlecode.com/svn/trunk/12_hour_time@13 af405dde-7b3e-0410-a8e9-8b16305dd6c8

--- lib/12_hour_time.rb
+++ lib/12_hour_time.rb
@@ -21,28 +21,32 @@ module ActionView::Helpers::DateHelper
   AM = 0
   PM = 1
 
-  def select_hour_with_ampm(datetime, options = {}) 
-    options[:twelve_hour] or return select_hour_without_ampm(datetime, options)
+  def select_hour_with_ampm(datetime, options = {}, html_options = {}) 
+    options[:twelve_hour] or
+      return select_hour_without_ampm(datetime, options, html_options)
 
-    hour = _12_hour(datetime)
+    val = _12_hour(datetime)
 
     if options[:use_hidden]
       return hidden_html(options[:field_name] || 'hour', hour, options)
     end
 
     hour_options = []
-    1.upto(12) do |val|
-      selected = (hour == val) ? ' selected="selected"' : ''
-      hour_options << %(<option value="#{leading_zero_on_single_digits(val)}"#{selected}>#{leading_zero_on_single_digits(val)}</option>\n)
+    1.upto(12) do |hour|
+       hour_options << ((val == hour) ?
+         content_tag(:option, leading_zero_on_single_digits(hour), :value => leading_zero_on_single_digits(hour), :selected => "selected") :
+         content_tag(:option, leading_zero_on_single_digits(hour), :value => leading_zero_on_single_digits(hour))
+       )
+       hour_options << "\n"
     end
 
-    select_html(options[:field_name] || 'hour', hour_options, options)
+    select_html(options[:field_name] || 'hour', hour_options, options, html_options)
   end
 
   alias_method_chain :select_hour, :ampm
 
 
-  def select_ampm(datetime, options = {})
+  def select_ampm(datetime, options = {}, html_options = {})
     val = ''
     if datetime 
       val = (datetime.kind_of?(Fixnum) ? datetime : datetime.hour) < 12 ? AM : PM
@@ -55,16 +59,17 @@ module ActionView::Helpers::DateHelper
     ampm_options = []
     label = { AM => 'AM', PM => 'PM' }
     [AM, PM].each do |meridiem|
-      selected = (meridiem == val) ? ' selected="selected"' : ''
-      ampm_options << %(<option value="#{meridiem}"#{selected}>#{label[meridiem]}</option>\n)
+      ampm_options << ((val == meridian) ?
+        content_tag(:option, label[meridiem], :value => meridiem, :selected => "selected") :
+        content_tag(:option, label[meridiem], :value => meridiem)
+      ampm_options << "\n"
     end
-
-    select_html(options[:field_name] || 'ampm', ampm_options, options)
+    select_html(options[:field_name] || 'ampm', ampm_options, options, html_options)
   end
 
-  def select_time_with_ampm(datetime = Time.now, options = {})
-    select = select_time_without_ampm(datetime, options)
-    select << select_ampm(datetime, options) if options[:twelve_hour]
+  def select_time_with_ampm(datetime = Time.now, options = {}, html_options = {})
+    select = select_time_without_ampm(datetime, options, html_options)
+    select << select_ampm(datetime, options, html_options) if options[:twelve_hour]
     select
   end
 
@@ -84,18 +89,18 @@ module ActionView::Helpers::DateHelper
 end
 
 class ActionView::Helpers::InstanceTag
-  def date_or_time_select_with_ampm(options)
+  def date_or_time_select_with_ampm(options, html_options = {})
     options[:twelve_hour] and not options[:discard_hour] or
-      return date_or_time_select_without_ampm(options)
+      return date_or_time_select_without_ampm(options, html_options)
 
     defaults = { :discard_type => true }
     options = defaults.merge(options)
 
     datetime = value(object)
-    datetime ||= Time.now unless options[:include_blank]
+    datetime ||= default_time_from_options(options[:default]) unless options[:include_blank]
 
-    date_or_time_select_without_ampm(options) +
-      select_ampm(datetime, options_with_prefix(6, options.merge(:use_hidden => options[:discard_hour])))
+    date_or_time_select_without_ampm(options, html_options) +
+      select_ampm(datetime, options_with_prefix(6, options.merge(:use_hidden => options[:discard_hour])), html_options)
   end
 
   alias_method_chain :date_or_time_select, :ampm
